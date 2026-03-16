ae93ed51ed58bf65fa58caa336e9fbe32a874501
Yevgeney <yevgeney1308@gmail.com>
2026-03-16 19:40:19 +0700

    header

---
+++ src/decimal.h
@@ -0,0 +1,17 @@
+#ifndef DECIMAL_CPP
+#define DECIMAL_CPP
+
+class Decimal_t {
+ private:
+  unsigned bit[4];
+
+ public:
+  Decimal_t(/* args */);
+  ~Decimal_t();
+};
+
+Decimal_t::Decimal_t(/* args */) {}
+
+Decimal_t::~Decimal_t() {}
+
+#endif
